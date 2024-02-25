6a48c5e37b8683014a59b8d71a2e9c75901c1b7b
Vladimir Panteleev <git@cy.md>
2024-02-25 08:51:55 +0000

    Examine all probabilities

--- company-llama.el
+++ company-llama.el
@@ -11,6 +11,9 @@
 (defcustom company-llama-api-url "http://127.0.0.1:8080/completion"
   "Llama API base url.")
 
+(defcustom company-llama-choice-threshold 0.1
+  "Probability threshold for displaying a choice.")
+
 (defun company-llama-fetch (prefix callback)
   "Fetch completion Candidates from Llama based on given PREFIX."
   (let ((url company-llama-api-url)
@@ -20,7 +23,7 @@
     (url-request-data
       (json-encode `(("prompt" . ,prefix)
 		     ("n_predict" . 32)
-		     ;; ("n_probs" . 5)
+		     ("n_probs" . ,(min 10 (round (/ 1.0 company-llama-choice-threshold))))
 		     ("temperature" . 0)
 		     ))))
     (url-retrieve
@@ -36,23 +39,52 @@
 
 (defun company-llama-candidate (data prefix)
   "Return a list of candidates from DATA."
-  ;; (let ((choices (cdr (assoc 'choices (assoc 'model data))))
-  ;;       (candidate '()))
-  ;;   (dolist (choice choices candidate)
-  ;;     (setq candidate 
-  ;;           (append candidate (split-string 
-  ;;       		       (cdr (assoc 'text (assoc 'finishing_choice choice))) "\\.")))))
-  (list
-   (concat prefix
-	   (cdr (assoc 'content data)))))
+  (let* ((s prefix)
+	 (tokens-vec (cdr (assoc 'completion_probabilities data)))
+	 (tokens (append tokens-vec nil))
+	 probs)
 
-(defun company-llama-candidates (callback prefix)
+    ;; Collect the tokens that the model is sure about.
+    (while (and tokens
+		;; Get the probs
+		(setq probs
+		      (cdr (assoc 'probs
+				  (car tokens))))
+		;; Convert from JSON to a cons cell
+		(setq probs
+		      (mapcar (lambda (item)
+				(cons
+				 (cdr (assoc 'tok_str item))
+				 (cdr (assoc 'prob item))))
+			      probs))
+		;; Filter out improbable completions
+		(setq probs
+		      (seq-filter
+		       (lambda (prob)
+			 (>= (cdr prob) company-llama-choice-threshold))
+		       probs))
+		;; Keep going if there is only one option.
+		(eq (length probs) 1))
+      (setq tokens (cdr tokens))
+      (setq s (concat s (car (car probs))))
+      (setq probs nil))
+
+    (if probs
+	;; Report branches as one candidate each.
+	(mapcar
+	 (lambda (prob)
+	   (concat s (car prob)))
+	 probs)
+      (list s))))
+
+(defun company-llama-candidates (callback)
   (company-llama-fetch
-   prefix
+   (company-llama-prefix)
    (lambda ()
-     (let ((data (company-llama-parse-response)))
-       (funcall callback
-		(company-llama-candidate data prefix))))))
+     (message "company-llama-candidates res")
+     (let* ((data (company-llama-parse-response))
+	    (candidates (company-llama-candidate data "")))
+       (funcall callback candidates)))))
 
 (defun company-llama-prefix ()
   (buffer-substring
@@ -67,9 +99,10 @@ COMMAND and ARG are as per the `company-backends' API."
   (interactive (list 'interactive))
   (cl-case command
     (interactive (company-begin-backend 'company-llama-backend))
-    (prefix (company-llama-prefix))
+    (prefix "")
     (candidates (cons :async (lambda (callback)
-			       (company-llama-candidates callback arg))))))
+			       (company-llama-candidates callback))))
+    (sorted t)))
 
 (add-to-list 'company-backends 'company-llama-backend)
 (setq company-backends (delete 'company-tabnine company-backends))
