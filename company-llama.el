328ad805d362e4a02bde17ab293dd77f6c885e19
Vladimir Panteleev <git@cy.md>
2024-02-25 17:31:17 +0000

    Use streaming API mode
    Show results ASAP.

--- company-llama.el
+++ company-llama.el
@@ -16,85 +16,128 @@
   "Probability threshold for displaying a choice."
   :type 'float)
 
-(defun company-llama-fetch (prefix callback)
+(defun company-llama--fetch (prefix event-handler)
   "Fetch completion Candidates from Llama based on given PREFIX."
-  (let ((url company-llama-api-url)
-    (url-request-extra-headers
-      `(("Content-Type" . "application/json")))
-    (url-request-method "POST")
-    (url-request-data
-      (json-encode `(("prompt" . ,prefix)
-		     ("n_predict" . 32)
-		     ("n_probs" . ,(min 10 (round (/ 1.0 company-llama-choice-threshold))))
-		     ("temperature" . 0)
-		     ))))
-    (url-retrieve
-     url
-     (lambda (_status)
-       (funcall callback))
-     nil t)))
+  (let* ((url company-llama-api-url)
+         (url-request-extra-headers
+          `(("Content-Type" . "application/json")))
+         (url-request-method "POST")
+         (url-request-data
+          (json-encode `(("prompt" . ,prefix)
+                         ("stream" . t)
+		         ("n_probs" . ,(min 10 (round (/ 1.0 company-llama-choice-threshold))))
+		         ("n_predict" . 32)
+		         ("temperature" . 0)
+		         )))
+         (buf (url-retrieve
+               url
+               (lambda (_status)
+                 (funcall event-handler 'done))
+               nil t))
+         (after-change-hook (lambda (_beg _end _len)
+                              (funcall event-handler 'change))))
+    (with-current-buffer buf
+      (add-hook 'after-change-functions after-change-hook nil t))))
 
-(defun company-llama-parse-response ()
-  "Parse the HTTP response in BUFFER."
-  (goto-char url-http-end-of-headers)
-  (json-read))
+(defun company-llama--make-data-handler (candidates-callback)
+  (let* ((s ""))
+    (lambda (data)
+      ;; (message "Got JSON: %S" data)
+      (if data
+          (let* ((tokens-vec (cdr (assoc 'completion_probabilities data)))
+	         (tokens (append tokens-vec nil))
+                 done)
+            (mapc
+             (lambda (token)
+               (let* (;; Get the probs
+                      (probs (cdr (assoc 'probs token)))
+                      ;; Convert from JSON to a cons cell
+                      (probs (mapcar (lambda (item)
+			               (cons
+			                (cdr (assoc 'tok_str item))
+			                (cdr (assoc 'prob item))))
+			             probs))
+		      ;; Filter out improbable completions
+		      (probs (seq-filter
+		              (lambda (prob)
+		                (>= (cdr prob) company-llama-choice-threshold))
+		              probs)))
 
-(defun company-llama-candidate (data prefix)
-  "Return a list of candidates from DATA."
-  (let* ((s prefix)
-	 (tokens-vec (cdr (assoc 'completion_probabilities data)))
-	 (tokens (append tokens-vec nil))
-	 probs)
+                 (cond
+                  ;; Already returned a result
+                  (done)
 
-    ;; Collect the tokens that the model is sure about.
-    (while (and tokens
-		;; Get the probs
-		(setq probs
-		      (cdr (assoc 'probs
-				  (car tokens))))
-		;; Convert from JSON to a cons cell
-		(setq probs
-		      (mapcar (lambda (item)
-				(cons
-				 (cdr (assoc 'tok_str item))
-				 (cdr (assoc 'prob item))))
-			      probs))
-		;; Filter out improbable completions
-		(setq probs
-		      (seq-filter
-		       (lambda (prob)
-			 (>= (cdr prob) company-llama-choice-threshold))
-		       probs))
-		;; Keep going if there is only one option.
-		(eq (length probs) 1))
-      (setq tokens (cdr tokens))
-      (setq s (concat s (car (car probs))))
-      (setq probs nil))
+                  ;; Only one likely candidate: remember and keep going
+                  ((eq (length probs) 1)
+                   (setq s (concat s (car (car probs)))))
 
-    (if (string-empty-p s)
-	;; Report branches as one candidate each.
-	(mapcar
-	 (lambda (prob)
-	   (concat s (car prob)))
-	 probs)
-      ;; Otherwise, just return the common prefix.
-      (list s))))
+                  ;; Multiple likely candidates
+                  (t
+                   (funcall candidates-callback
+                            (if (string-empty-p s)
+	                        ;; Report branches as one candidate each.
+	                        (mapcar
+	                         (lambda (prob)
+	                           (car prob))
+	                         probs)
+                              ;; Otherwise, just return the common prefix.
+                              (list s)))
+                   (setq done t)))))
+             tokens)
+            ;; Return nil if we don't want any more data.
+            (not done))
+        ;; data is nil (EOF):
+        (funcall candidates-callback
+                            (if (string-empty-p s)
+                                nil
+                              (list s)))))))
 
-(defun company-llama-candidates (callback)
-  (company-llama-fetch
-   (company-llama-prefix)
-   (lambda ()
-     (message "company-llama-candidates res")
-     (let* ((data (company-llama-parse-response))
-	    (candidates (company-llama-candidate data "")))
-       (funcall callback candidates)))))
+(defun company-llama--make-url-event-handler (candidates-callback)
+  (let* ((data-handler (company-llama--make-data-handler candidates-callback))
+         last-pos done)
+    (lambda (event-type)
+      (unless done
+        (cl-case event-type
+          (change
+           ;; Check if we have headers, and can begin parsing data.
+           (when (and (null last-pos)
+                      (boundp 'url-http-end-of-headers)
+	              url-http-end-of-headers)
+             (setq last-pos url-http-end-of-headers))
 
-(defun company-llama-prefix ()
+           ;; Parse new data.
+           (when last-pos
+             (save-excursion
+               (save-match-data
+                 (while
+                     (condition-case nil
+                         (progn
+                           (goto-char last-pos)
+                           (re-search-forward "data: ")
+                           (let ((json (json-read)))
+                             (if (funcall data-handler json)
+                                 t
+                               (setq done t)
+                               ;; TODO: cancel the request.
+                               nil)))
+                       (t
+                        nil))
+                   (setq last-pos (point)))))))
+          (done
+           (funcall data-handler nil)
+           (setq done t)))))))
+
+(defun company-llama--prefix ()
   (buffer-substring
    (max
     (point-min)
     (- (point) 16384))
    (point)))
+
+(defun company-llama--candidates (candidates-callback)
+  (company-llama--fetch
+   (company-llama--prefix)
+   (company-llama--make-url-event-handler candidates-callback)))
 
 (defun company-llama-backend (command &optional _arg &rest _ignored)
   "Company Mode backend function for Llama.
@@ -103,8 +146,7 @@ COMMAND and ARG are as per the `company-backends' API."
   (cl-case command
     (interactive (company-begin-backend 'company-llama-backend))
     (prefix "")
-    (candidates (cons :async (lambda (callback)
-			       (company-llama-candidates callback))))
+    (candidates (cons :async #'company-llama--candidates))
     (sorted t)))
 
 (provide 'company-llama)
