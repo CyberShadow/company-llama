521c91db4862b517101777b49d5a0c344093dd2c
Vladimir Panteleev <git@cy.md>
2024-02-28 08:37:36 +0000

    Add standard header + license

--- company-llama.el
+++ company-llama.el
@@ -1,4 +1,36 @@
-;;; -*- lexical-binding: t -*-
+;;; company-llama.el --- Glue between company-mode and llama.cpp   -*- lexical-binding: t -*-
+
+;; Version: 0.1.0
+;; Author: Vladimir Panteleev
+;; Url: https://github.com/CyberShadow/company-llama
+;; Package-Requires: ((emacs "24.3") (company "0.9.13"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; This package implements a company-mode backend which fetches
+;; completion candidates from a llama.cpp instance's completions
+;; endpoint.
+
+;; For more information, please see the accompanying README.md file.
+
+;;; Code:
 
 (require 'company)
 (require 'json)
@@ -268,3 +300,4 @@ COMMAND and ARG are as per the `company-backends' API."
     (sorted t)))
 
 (provide 'company-llama)
+;;; company-llama.el ends here
